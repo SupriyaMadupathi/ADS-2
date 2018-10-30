902a53b450cedf02dc57623f56e0f100b72eb28b
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-10-30 20:33:23 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,17 +1,34 @@
-
+/* To import scanner class*/
 import java.util.Scanner;
-
+/**
+ * Class for percolation.
+ */
 class Percolation {
-
+/**
+ * variable to keep count value.
+ */
     private int cnt;
-    
+    /**
+     * to take 2 dimentional array.
+     */
     private int[][] grid;
-    
+    /**
+     * { to keep size }.
+     */
     private int size;
-    
+    /**
+     * object for weighted addEdge class.
+     */
     private Graph cd;
+    /**
+     * cc object.
+     */
     private CC cc;
-    
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     */
     Percolation(final int n) {
         grid = new int[n][n];
         cnt = 0;
@@ -19,7 +36,12 @@ class Percolation {
         cc = new CC(cd);
         this.size = n;
     }
-    
+    /**
+     * function to check whether the element has to be connected or not.
+     *
+     * @param      row   The value of row
+     * @param      col   The value of column
+     */
     void open(final int row, final int col) {
         grid[row][col] = 1;
         cnt++;
@@ -45,41 +67,78 @@ class Percolation {
                     component(row, col + 1), component(row, col));
         }
     }
-    
+    /**
+     * to get the component at the particular row and column.
+     *
+     * @param      i     row index is given.
+     * @param      j     column index is given
+     *
+     * @return     return type is int
+     */
     int component(final int i, final int j) {
         return (i) * size + j;
     }
-    
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
     boolean isOpen(final int row, final int col) {
         return grid[row][col] == 1;
     }
-    
+    /**
+     * Determines if full.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if full, False otherwise.
+     */
     boolean isFull(final int row, final int col) {
         return grid[row][col] == 0;
     }
-    
+    /**
+     * to get the number of open sites.
+     *
+     * @return     integer is returned.
+     */
     int numberofopensites() {
         return cnt;
     }
-    
+    /**
+     * function to check if the grid percolates or not.
+     *
+     * @return     True if percolates, False otherwise.
+     */
     boolean percolates() {
         cc = new CC(cd);
         return cc.connected(0, (size * size) + 1);
     }
 }
-
+/**
+ * Class for solution.
+ */
 class Solution {
-    
+    /**
+     * Constructs the object.
+     */
     protected Solution() {
 
     }
-    
+    /**
+     * function of main.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int vertices = sc.nextInt();
+        Scanner s = new Scanner(System.in);
+        int vertices = s.nextInt();
         Percolation wc = new Percolation(vertices);
-        while (sc.hasNext()) {
-        wc.open(sc.nextInt() - 1, sc.nextInt() - 1);
+        while (s.hasNext()) {
+        wc.open(s.nextInt() - 1, s.nextInt() - 1);
         }
         System.out.println(wc.percolates());
     }
