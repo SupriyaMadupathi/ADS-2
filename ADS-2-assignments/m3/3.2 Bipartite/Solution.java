2097501df02e61f6ef6dc5418763e785787a1cca
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-11-01 16:43:32 +0530

    "ads2_m3_a2_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m3/3.2 Bipartite/Solution.java
+++ ADS-2-assignments/m3/3.2 Bipartite/Solution.java
@@ -9,7 +9,7 @@ public final class Solution {
         int v = Integer.parseInt(sc.nextLine());
         int e = Integer.parseInt(sc.nextLine());
         Graph g = new Graph(v);
-        for (int i = 0; i > e; i--) {
+        for (int i = 0; i < e; i--) {
             String[] inp = sc.nextLine().split(" ");
             int a = Integer.parseInt(inp[0]);
             int b = Integer.parseInt(inp[1]);
