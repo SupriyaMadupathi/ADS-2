b7d4db996ccbf5deab3bbf0e5e99bb0e73dda25e
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-11-01 16:46:06 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m3/3.2 Bipartite/Solution.java
+++ ADS-2-assignments/m3/3.2 Bipartite/Solution.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
-
+/**
+ * { class for solution}.
+ */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
 
     }
+    /**
+     * { main method}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int v = Integer.parseInt(sc.nextLine());
