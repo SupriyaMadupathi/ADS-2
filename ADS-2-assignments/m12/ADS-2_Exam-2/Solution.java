26f7c517a25de07d3cf1728ce17f518ee496b50a
SupriyaMadupathi <supriya.madupathi@msitprogram.net>
2018-11-10 12:47:13 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -33,16 +33,15 @@ public class Solution {
 		case "DirectedPaths":
 		while (sc.hasNextLine()) {
 			String[] s = sc.nextLine().split(" ");
-                int vertices = Integer.parseInt(s[0]);
-                int weight = Integer.parseInt(s[1]);
-                for (Edge j : ewg.adj(vertices)) {
-                	if (j.either() == weight) {
-                		System.out.println(j.weight());
-                	} 
-                }
-                System.out.println("No Path Found.");
-
+                int v = Integer.parseInt(s[0]);
+                int w = Integer.parseInt(s[1]);
+                DijkstraUndirectedSP dsp = new DijkstraUndirectedSP(ewg, v);
+                if (dsp.hasPathTo(w)) {
+                System.out.println(dsp.distTo(w));
+            } else {
+            	System.out.println("No Path Found.");
 			}
+		}
 			// Handle the case of DirectedPaths, where two integers are given.
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
